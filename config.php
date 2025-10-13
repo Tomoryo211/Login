6d06f49cdf059af5b9541530e3b410b3210604bf
Tomoryo211 <ryo.tomo.211@gmail.com>
2025-10-13 10:45:35 +0900

    DB接続

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+// DB関係
+const DB_DRIVER = "mysql";
+const DB_NAME = "rtomoduka";
+const DB_HOST = "localhost";
+const DB_USER = "rtomoduka";
+const DB_PASS = "eccMyAdmin";
+const DB_CHARSET = "utf8mb4";
+const DB_DSN = DB_DRIVER . ":dbname=" . DB_NAME . ";host=" . DB_HOST . ";charaset=" . DB_CHARSET;
+
+const TB_LOGIN = "Login";
+const TB_WORK = "app1_WORKS";
+?>
---
+++ datebase/Login.sql
@@ -0,0 +1,7 @@
+
+
+CREATE TABLE Login(
+    id INT AUTO_INCRMENT PRIMARY  KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) 
+)
