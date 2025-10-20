0e8556a9a81265975195102bf1be73f8ac73ce09
Tomoryo211 <ryo.tomo.211@gmail.com>
2025-10-20 11:19:32 +0900

    session設定

--- Login.php
+++ Login.php
@@ -9,5 +9,21 @@ try{
     die("データベース接続失敗: " . $e->getMessage());
 }
 
-if()
+if($_SERVER["REQUEST_METHOD"] === "POST");{
+    $email = $_POST["email"];
+    $pass = $_POST["password"];
+
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
+    $stmt -> execute(["email"]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if($user && password_verify($pass, $user["password"])){
+        $_SESSION["user_id"] = $user["id"];
+        $_SESSION["email"] = $user["email"];
+        header("Location: dashboard.php");
+        exit;
+    }else{
+        echo "メールアドレスまたはパスワードが間違っています。";
+    }
+}
 ?>
