c06c987789b4c7da6d04b86ba20a2a83a158be5d
Tomoryo211 <ryo.tomo.211@gmail.com>
2025-10-26 02:10:02 +0900

    ログイン後のページ

--- users/Loginrear.php
+++ users/Loginrear.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 session_start();
-if(!isset($_SESSION["user_id"])){
+if (!isset($_SESSION["user_id"])) {
     header("Location: Login.php");
     exit;
 }
@@ -9,13 +9,13 @@ if(!isset($_SESSION["user_id"])){
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <title>ダッシュボード</title>
+    <title>ようこそ画面</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-50 flex items-center justify-center min-h-screen">
     <div class="bg-white p-10 rounded-xl shadow-lg text-center">
         <h1 class="text-2xl font-bold mb-4">ようこそ、<?php echo htmlspecialchars($_SESSION["email"]); ?> さん！</h1>
-        <a href="Logout.php" class="text-blue-500 hover:underline">ログアウト</a>
+        <a href="Login.php" class="text-blue-500 hover:underline">戻る</a>
     </div>
 </body>
 </html>
